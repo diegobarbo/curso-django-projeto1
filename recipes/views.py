11d08d4960dcb88f27b572e150fa2d1e5e512566
Diego Barbosa <diegobarbox@gmail.com>
2022-01-20 21:39:57 -0300

    caminhos templates

--- recipes/views.py
+++ recipes/views.py
@@ -2,7 +2,9 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 def home(request):
-    return render(request, 'home.html')
+    return render(request, 'recipes/home.html', context={
+        'name': 'Diego Barbosa',
+    })
 
 
 def contato(request):
